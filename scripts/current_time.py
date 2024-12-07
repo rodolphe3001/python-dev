47ad31f5e4c8d5f767e00e3cad39c7910e498705
rodolphe <rodolphe@rodolphe3001.fr>
2024-12-07 10:05:34 +0100

    working on python scripts

---
+++ scripts/current_time.py
@@ -0,0 +1,10 @@
+import time
+#from time import sleep 
+
+from datetime import datetime
+now = datetime.now()  
+
+print ("%s/%s/%s %s:%s:%s" % (now.month,now.day,now.year,now.hour,now.minute,now.second)) 
+
+time.sleep(1)
+#sleep(1)
